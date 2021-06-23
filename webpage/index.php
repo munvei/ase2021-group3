a1dea6628d46e016f1611324780a08598c9068ef
Fuji_warota <255116p@gs.kochi-tech.ac.jp>
2021-06-23 19:16:25 +0900

    現在のアラーム予約時刻の表示とカーテン操作ボタンを押した際に任意のjsを実行するページ

--- webpage/index.php
+++ webpage/index.php
@@ -10,7 +10,22 @@
     <h1 class="center">ACOS</h1>
     <center style="font-size:100%" id="TimeArea"></center>
     <div>アラーム設定</div>
+    <?php 
+      if(isset($_POST['alarm_time'])){
+        include('create_file.php');
+      }
+      $file = "test.txt";
+      $line = file_get_contents($file);
+      echo "現在の設定時刻($line)"
+    ?>
+    <form action="#" method="POST">
+      <p>アラーム時刻:<br>
+      <input type="time" name="alarm_time"></p>
+      <p><input type="submit" value="決定"></p>
+    </form>
     <div>カーテン操作</div>
+    <input type="button" value="操作" onclick="CurtainMove();">
     <script type="text/javascript" src="./script.js"></script>
   </body>
 </html>
+
